f0faa40112450c5018cf96f1fd523d98bbab7225
trasheyphnsum <trasheyphuentsho04@gmail.com>
2021-01-10 15:34:03 +0600

    sixth commit

--- resources/views/hire/login.blade.php
+++ resources/views/hire/login.blade.php
@@ -3,7 +3,7 @@
 <div class="row justify-content-center">
     <div class="col-4">
     <h1>Login User</h1>
-        <form method="post" action="register">
+        <form method="post" action="login">
         {{ @csrf_field() }}
             <div class="form-group">
                 <label>Email address</label>
