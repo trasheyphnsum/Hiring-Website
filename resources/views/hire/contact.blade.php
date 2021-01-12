24c6bcfe92f64f2caa54ee96bffc5e855ae2378f
trasheyphnsum <trasheyphuentsho04@gmail.com>
2021-01-12 16:05:18 +0600

    Changes

--- resources/views/hire/contact.blade.php
+++ resources/views/hire/contact.blade.php
@@ -1,29 +1,32 @@
 @extends('layout')
 @section('content') 
-<div class="row justify-content-center">
-    <div class="col-4">
-        <h1>Contact Us</h1>
-        <p><strong>Got question? We would love to solve your problem. Send us a message and 
-        we will repond as soon as possible</strong></p>
-        <form method="get" action="contact">
-        @csrf
-            <div class="form-group">
-                <label>Name</label>
-                <input type="text" class="form-control" name ="Name" placeholder="Enter name">
-            </div>
-            <div class="form-group">
-                <label>Email address</label>
-                <input type="email" class="form-control" name ="Email" placeholder="Enter email">
-            </div>
-            <div class="form-group">
-                <label >Phone Number</label>
-                <input type="phone" class="form-control" name="Phone_Number"  placeholder="Enter Phone Number">
-            </div>
-            <div class="row-8">
-            <p>Message</p><textarea name="message" rows="5" cols="57"></textarea><br/>
-            </div>
-            <br><button type="submit" class="btn btn-primary">Send</button>
-        </form>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <p><strong>Got question? We would love to solve your problem. Send us a message and 
+            we will repond as soon as possible</strong></p>
+            <form method="get" action="contact">
+            @csrf
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" class="form-control" name ="Name" placeholder="Enter name">
+                </div>
+                <div class="form-group">
+                    <label>Email address</label>
+                    <input type="email" class="form-control" name ="Email" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label >Phone Number</label>
+                    <input type="phone" class="form-control" name="Phone_Number"  placeholder="Enter Phone Number">
+                </div>
+                <div class="row-8">
+                <p>Message</p><textarea name="message" rows="5" cols="57"></textarea><br/>
+                </div>
+                <br><button type="submit" class="btn btn-primary">Send</button>
+            </form>
+        </div>
+    </div>
     </div>
 </div>
 @endsection
--- resources/views/hire/login.blade.php
+++ resources/views/hire/login.blade.php
@@ -1,30 +1,26 @@
 @extends('layout')
 @section('content')
-<div class="card text-center">
-    <div class="card border-primary mb-4">
-        <div class="card-header"> 
-            <h5 class="card-title">User Login</h5>
-        </div>
-        <div class="card-body">
-            <div class="row justify-content-center">
-                <div class="col-4">
-                    <form method="post" action="login">
-                        {{ @csrf_field() }}
-                        <div class="form-group">
-                        <label>Email</label>
-                            <input type="email" class="form-control" name ="Email" placeholder="Enter email">
-                        </div>
-                        <div class="form-group">
-                            <label >Password</label>
-                            <input type="password" class="form-control" name="Password"  placeholder="Enter Password">
-                        </div>
-                        <button type="submit" class="btn btn-primary">sign in</button>
-                    </form> 
-                    <p>Not a member? <a href="/register">Sign Up</a></p>
-                    <p>Forgot <a href="#">Password?</a></p>  
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">User Login</h1>
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <form method="post" action="login">
+                {{ @csrf_field() }}
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" class="form-control" name ="Email" placeholder="Enter email">
                 </div>
-            </div> 
+                <div class="form-group">
+                    <label >Password</label>
+                    <input type="password" class="form-control" name="Password"  placeholder="Enter Password">
+                </div>
+                <button type="submit" class="btn btn-primary">sign in</button>
+            </form> 
+            <p>Not a member? <a href="/register">Sign Up</a></p>
+            <p>Forgot <a href="#">Password?</a></p>  
         </div>
+    </div> 
     </div>
 </div>
 @endsection
