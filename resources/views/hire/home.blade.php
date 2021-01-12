573e7bbea7e1aebc3cf0ca1e863e983c030e45c2
trasheyphnsum <trasheyphuentsho04@gmail.com>
2021-01-12 20:41:51 +0600

    remove home content

--- resources/views/hire/home.blade.php
+++ resources/views/hire/home.blade.php
@@ -6,36 +6,4 @@
     <button type="button" class="close" data-dismiss="alert">&times;</button>
 </div>
 @endif
-@if(Session::get('status'))
-<div class="alert alert-success alert-dismissible fade show">
-    {{Session::get('status')}}
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
-</div>
-@endif
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="guru.jpg" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="guru.jpg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="guru.jpg" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
 @endsection
---
+++ resources/views/hire/reservation.blade.php
@@ -0,0 +1,31 @@
+@extends('layout')
+@section('content')
+<div class="card hovercard">
+                <div class="cardheader">               
+					<div class="avatar">
+						<img alt="" src="<?php echo $record['image']; ?>">
+					</div>
+				 </div>
+                <div class="card-body info">
+                    <div class="title">
+                        <a href="#"><?php echo $record['name']; ?></a>
+                    </div>
+					<div class="desc"> <a target="_blank" href="<?php echo $record['website']; ?>"><?php echo $record['website']; ?></a></div>		
+                    <div class="desc"><?php echo $record['description']; ?></div>      
+					<div class="desc"><?php echo $record['address']; ?></div>								
+                </div>
+                <div class="card-footer bottom">
+                    <a class="btn btn-primary btn-twitter btn-sm" href="<?php echo $record['twitter']; ?>">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a class="btn btn-danger btn-sm" rel="publisher"
+                       href="<?php echo $record['gplus']; ?>">
+                        <i class="fa fa-google-plus"></i>
+                    </a>
+                    <a class="btn btn-primary btn-sm" rel="publisher"
+                       href="<?php echo $record['facebook']; ?>">
+                        <i class="fa fa-facebook"></i>
+                    </a>                    
+                </div>
+            </div>
+@endsection
